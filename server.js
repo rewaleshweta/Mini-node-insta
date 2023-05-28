49e6c97422e421a14860e2ae4e43d7b75fdbe7e7
rewaleshweta <rewaleshweta5@gmail.com>
2023-05-28 19:59:55 +0530

    instclone backend functinality is added

---
+++ server.js
@@ -0,0 +1,22 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const app=express()
+const dotenv=require("dotenv")
+const cors=require("cors")
+app.use(express.json())
+app.use(cors({
+    origin:"http://localhost:3000"
+}))
+const PORT=process.env.PORT || 8080
+dotenv.config()
+mongoose.connect(process.env.MONGO_URI)
+mongoose.connection.on("connected",()=>{
+    console.log("congrats!!! mongodb connected successfully")
+})
+mongoose.connection.on("error",(err)=>{
+    console.log("sorry mongodb not connected",err)
+})
+app.use(require("./routes/Post"))
+app.listen(PORT,()=>{
+console.log("app runnning on port 8080")
+})
